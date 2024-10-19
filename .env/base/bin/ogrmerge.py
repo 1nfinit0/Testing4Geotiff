f4bb56ec61bd2189373383c5b3d9fbb3217a7159
Tobi <noggnzzz@gmail.com>
2024-10-19 01:40:58 -0500

    GeoTiff test manipulation

---
+++ .env/base/bin/ogrmerge.py
@@ -0,0 +1,12 @@
+#!/home/tobi/Desktop/gis/sandox/notebook/.env/base/bin/python3
+
+import sys
+
+from osgeo.gdal import deprecation_warn
+
+# import osgeo_utils.ogrmerge as a convenience to use as a script
+from osgeo_utils.ogrmerge import *  # noqa
+from osgeo_utils.ogrmerge import main
+
+deprecation_warn("ogrmerge")
+sys.exit(main(sys.argv))
